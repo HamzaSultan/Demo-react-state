03ec36dde881c686ebee958bd4f435469037b3c7
HamzaSultan <56472325+HamzaSultan@users.noreply.github.com>
2020-06-21 16:19:45 +0500

    basic react state

---
+++ src/Room.js
@@ -0,0 +1,47 @@
+import React, {useState} from 'react';
+
+
+function Room() {
+
+// const state = useState (true);
+// const isLit = state [0]; 
+// const setLit = state [1];
+// console.log("State",state);
+
+
+//function updateLit() {
+//console.log("Button clicked");
+//setLit(!isLit);
+
+//}
+
+// function increaseAge() {
+//     console.log(" increaseAge Button clicked");
+//     setAge(++age);
+//}
+
+
+let [isLit, setLit] = useState(false);
+    let [age , setAge] = useState(21);
+
+
+    return (
+        <div className={`room ${isLit? "lit": "dark"}`}> 
+            This Room is {isLit? "lit": "dark"}
+            <br/>
+            Age: {age}
+            <br/>
+            <button onClick={()=> setLit(!isLit) } >Toggle Light</button>
+            <br/>
+            <button onClick={ ()=> {
+                            console.log("Increase age arrow function");
+                            setAge(++age);
+                            }}>
+                Increase Age
+            </button>
+        </div>
+        );
+
+  
+  }
+export default Room;
